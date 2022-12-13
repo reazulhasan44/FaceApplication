f453f7238905832520e17e11fcd2e5e702884016
bonkhead44 <reazshovon44@gmail.com>
2022-12-14 00:02:16 +0600

    Add project files.

---
+++ FaceApiTest/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using FaceApiTest;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+var imagePath = @"oscars-2017.jpg";
+var urlAddress = "http://localhost:6001/api/faces";
+ImageUtility imgUtil = new ImageUtility();
+var bytes = imgUtil.ConvertToBytes(imagePath);
+List<byte[]> faceList = null;
+var byteContent = new ByteArrayContent(bytes);
+byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+using (var httpClient = new HttpClient())
+{
+    using (var response = await httpClient.PostAsync(urlAddress, byteContent))
+    {
+        string apiResponse = await response.Content.ReadAsStringAsync();
+        faceList = JsonConvert.DeserializeObject<List<byte[]>>(apiResponse);
+    }
+}
+if (faceList != null && faceList.Count > 0)
+{
+    for (int i = 0; i < faceList.Count; i++)
+    {
+        imgUtil.FromBytesToImage(faceList[i], "face" + i);
+
+    }
+}
---
+++ FacesApi/Controllers/FacesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenCvSharp;
+
+namespace FacesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FacesController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<List<byte[]>> ReadFaces()
+        {
+            using (var ms = new MemoryStream(2048))
+            {
+                await Request.Body.CopyToAsync(ms);
+                var faces = Getfaces(ms.ToArray());
+                return faces;
+            }
+        }
+
+        private List<byte[]> Getfaces(byte[] image)
+        {
+            Mat src = Cv2.ImDecode(image, ImreadModes.Color);
+
+            src.SaveImage("image.jpg", new ImageEncodingParam(ImwriteFlags.JpegProgressive, 255));
+            var file = Path.Combine(Directory.GetCurrentDirectory(), "CascadeFile", "haarcascade_frontalface_default.xml");
+            var faceCascade = new CascadeClassifier();
+            faceCascade.Load(file);
+
+            var faces = faceCascade.DetectMultiScale(src, 1.1, 6, HaarDetectionTypes.DoRoughSearch, new Size(60, 60));
+            var facesList = new List<byte[]>();
+            int j = 0;
+            foreach (var rect in faces)
+            {
+                var faceImage = new Mat(src, rect);
+                facesList.Add(faceImage.ToBytes(".jpg"));
+                faceImage.SaveImage("face" + j + ".jpg", new ImageEncodingParam(ImwriteFlags.JpegProgressive, 255));
+                j++;
+            }
+            return facesList;
+        }
+    }
+}
