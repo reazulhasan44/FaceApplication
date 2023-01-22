984d76875d20fc31d8f4332b5f9680e93a0b34b4
bonkhead44 <reazshovon44@gmail.com>
2023-01-23 00:29:38 +0600

    05/05

--- Faces.Web/Faces.WebMvc/Controllers/HomeController.cs
+++ Faces.Web/Faces.WebMvc/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
 ﻿using Faces.WebMvc.Models;
+using Faces.WebMvc.ViewModels;
+using MassTransit;
+using Messaging.InterfacesConstants.Commands;
+using Messaging.InterfacesConstants.Constants;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +11,12 @@ namespace Faces.WebMvc.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBusControl _iBusControl;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IBusControl iBusControl)
         {
             _logger = logger;
+            _iBusControl = iBusControl;
         }
 
         public IActionResult Index()
@@ -18,6 +24,37 @@ namespace Faces.WebMvc.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult RegisterOrder()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterOrder(OrderViewModel orderViewModel)
+        {
+            MemoryStream ms = new MemoryStream();
+            using (var uploadedFile = orderViewModel.File.OpenReadStream())
+            {
+                await uploadedFile.CopyToAsync(ms);
+            }
+            orderViewModel.ImageData = ms.ToArray();
+            orderViewModel.PictureUrl = orderViewModel.File.FileName;
+            orderViewModel.OrderId = Guid.NewGuid();
+            var sendToUri = new Uri($"{RabbitMqMassTransitConstants.RabbitMqUri}" + $"/{RabbitMqMassTransitConstants.RegisterOrderCommandQueue}");
+            var endPoint = await _iBusControl.GetSendEndpoint(sendToUri);
+            await endPoint.Send<IRegisterOrderCommand>(
+                new
+                {
+                    orderViewModel.OrderId,
+                    orderViewModel.UserEmail,
+                    orderViewModel.ImageData,
+                    orderViewModel.PictureUrl
+                });
+            ViewData["OrderId"] = orderViewModel.OrderId;
+            return View("Thanks");
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ Faces.Web/Faces.WebMvc/ViewModels/OrderViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Faces.WebMvc.ViewModels
+{
+    public class OrderViewModel
+    {
+        [Display(Name = "Order Id")]
+        public Guid OrderId { get; set; }
+
+        [Display(Name = "Email")]
+        public string UserEmail { get; set; }
+
+        [Display(Name = "Image File")]
+        public IFormFile File { get; set; }
+
+        [Display(Name = "Picture Url")]
+        public string PictureUrl { get; set; }
+
+        [Display(Name = "Order Status")]
+        public string OrderStatus { get; set; }
+
+        public byte[] ImageData { get; set; }
+        public string ImageString { get; set; }
+    }
+}
